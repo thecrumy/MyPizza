5fc9f816edfcc3a44d039779357f2b272b923923
Jim <jim.crum@nationwide.com>
2020-12-22 12:28:48 -0500

    end of section 3 - designing the ui with widgets

--- flutter_app/lib/ui/home.dart
+++ flutter_app/lib/ui/home.dart
@@ -19,7 +19,7 @@ class Home extends StatelessWidget {
                     // font weight
                   ),
               )),
-              Expanded(child:Text("Tomato, Mozz, Basil", 
+              Expanded(child:Text("Tomato, Mozz, garlic",
                   textDirection: TextDirection.ltr,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -54,6 +54,7 @@ class Home extends StatelessWidget {
             //  END OF THE EXPANDED SECTION
             /// X
             PizzaImageWidget(),
+            OrderButton(),
             ],
         )
         )
@@ -68,5 +69,31 @@ class PizzaImageWidget extends StatelessWidget{
     Image image = Image(image: pizzaAssest, width: 400.0, height: 400.0,);
     return Container(child: image);
   }
+}
+class OrderButton extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    var button = Container(
+      margin: EdgeInsets.only(top: 50.0),
+      child: RaisedButton(
+        child: Text("Order your Pizza!"),
+        color: Colors.lightGreen,
+        elevation: 5.0,
+        onPressed: () {
+          order(context);
+        },
+      ),
+    );
+    return button;
+  }
 
+  void order(BuildContext context){
+    var alert = AlertDialog(
+      title: Text("Order Completed"),
+      content: Text("Thanks for your Order"),
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => alert);
+  }
 }
